1ce6631b3df7fe61ad565524a420a9ff026129bf
Gustavo Peralta <1719110275@utectulancingo.edu.mx>
2020-06-07 04:04:53 +0000

    programa Estudiante, funcionando

---
+++ semana_4/estudiante.py
@@ -0,0 +1,44 @@
+class Estudiante:
+    #atributos#
+    materias=9
+    color_uniforme="Azul"
+    escuela="UTEC"
+    maestros=8
+    aula=23
+
+    #metodos#
+    def __init__(self):
+        print("Estudiante")
+
+    def estudiar(self):
+        print("Estudiar medicina")
+
+    def aprender(self):
+        print("Aprender de sus maestros")
+
+
+class Gustavo(Estudiante):
+    edad=18
+    promedio=8
+
+    def estudiar(self):
+        print("Estudio para mis examenes")
+    def aprender(self):
+        print("Cada dia aprendo algo nuevo")
+
+objeto_estudiante=Estudiante()
+objeto_estudiante.estudiar()
+objeto_estudiante.aprender()
+
+objeto_gustavo=Gustavo()
+objeto_gustavo.estudiar()
+objeto_gustavo.aprender()
+
+gustavo=Gustavo()
+print(gustavo.materias)
+print(gustavo.color_uniforme)
+print(gustavo.escuela)
+print(gustavo.maestros)
+print(gustavo.aula)
+print(gustavo.edad)
+print(gustavo.promedio)
