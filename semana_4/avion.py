d7bbaeb288d112ef4dcdce5700e9c69f1eae6ed3
Gustavo Peralta <1719110275@utectulancingo.edu.mx>
2020-06-07 04:34:01 +0000

    Concorde funcionando

---
+++ semana_4/avion.py
@@ -0,0 +1,48 @@
+class Avion:
+    #atributos#
+    alas=2
+    alerones=3
+    turbinas=5
+    piloto=2
+    destino="Miami"
+
+    #metodos#
+    def __init__(self):
+        print("Avion")
+
+    def volar(self):
+        print("El avion vuela muy rapido")
+
+    def aterrizar(self):
+        print("El avion acaba de aterrizar")
+
+class Concorde(Avion):
+    velocidad="2.179 km/h"
+    motor="Rolls-Royce/Snecma Olympus 593"
+
+    def __init__(self):
+        print("Constructor Concorde")
+
+    def volar(self):
+        print("El Concorde rompe la barrera del sonido")
+    def aterrizar(self):
+        print("El Concorde aterriza silenciosamente")
+
+
+objeto_avion=Avion()
+objeto_avion.volar()
+objeto_avion.aterrizar()
+
+objeto_concorde=Concorde()
+objeto_concorde.volar()
+objeto_concorde.aterrizar()
+
+concorde=Concorde()
+print(concorde.alas)
+print(concorde.alerones)
+print(concorde.turbinas)
+print(concorde.piloto)
+print(concorde.destino)
+print(concorde.velocidad)
+print(concorde.motor)
+  
